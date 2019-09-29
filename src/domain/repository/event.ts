c45ef5efe4b18c384fb60c7fc1b2afbcb23a2f6f
KatsuyaAkasaka <akasaka_katsuya@cyberagent.co.jp>
2019-09-29 16:30:46 +0900

    :+1: repository層修正

--- src/domain/repository/event.ts
+++ src/domain/repository/event.ts
@@ -1,13 +1,3 @@
-import { ReturnType } from "../domain";
-
-export function NewRepository(): ReturnType {
-    const newReturnType: ReturnType = {
-        statusCode: 200,
-        body: JSON.stringify({message: "hello"})
-    };
-    return newReturnType
-}
-
 export abstract class IEventRepository {
     abstract async getAll(): Promise<string>
 }
