fe59d4f00187657675651a4c3a1fd4800836a26e
KatsuyaAkasaka <akasakatora1208@gmail.com>
2020-04-25 01:49:12 +0900

    Revert "edit req param"
    This reverts commit 60e90f1d948c2aa09b839bbdd8c021b0aeb38211.

--- src/usecase/input/event.ts
+++ src/usecase/input/event.ts
@@ -2,7 +2,7 @@ import { getEventsReq } from '../../domain/event'
 // match検索出来るように正規表現形式にする
 export function parseToInput (req: any): getEventsReq {
   let topicstr: string[] = req.topics
-  let pref: number = req.place
+  let pref: number = req.pref
   if (pref === 0) topicstr.push('リモート');
   const topics: RegExp = new RegExp(topicstr.map(t => escapeRegExp(t)).join("|"), 'i')
   const inputEvents: getEventsReq = {
